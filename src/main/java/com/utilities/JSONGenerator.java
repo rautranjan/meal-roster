ce5a848e26a8e23c81851c99884fcab35b9f1834
Ranjan Raut <ranjannandkumar.raut@charter.com>
2019-07-17 17:59:16 -0600

    Created file generatng utilties

---
+++ src/main/java/com/utilities/JSONGenerator.java
@@ -0,0 +1,18 @@
+package com.utilities;
+
+import com.meal.Home;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONGenerator {
+
+    JSONObject schedule = new JSONObject();
+
+    void createJson(Home home){
+
+        //Adding sequence
+        JSONArray sequence = new JSONArray();
+
+    }
+
+}
