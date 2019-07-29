807decfa513aaa268b42fc1eb43ae3bab95d4aa3
Ranjan <ranjan.raut18@gmail.com>
2019-07-28 23:01:36 -0600

    Adding changes to schedule

--- src/main/java/com/meal/Week.java
+++ src/main/java/com/meal/Week.java
@@ -28,7 +28,7 @@ public class Week {
         this.monday = new Shift(home.getRecent()[3], home.getRecent()[4], home.getRecent()[1]);
         this.tuesday = new Shift(home.getRecent()[0], home.getRecent()[1], home.getRecent()[3]);
         this.wednesday = new Shift(home.getRecent()[2], home.getRecent()[3], home.getRecent()[1]);
-        this.thursday = new Shift(home.getRecent()[0], home.getRecent()[4], home.getRecent()[3]);
+        this.thursday = new Shift(home.getRecent()[4], home.getRecent()[0], home.getRecent()[3]);
         this.friday = new Shift(home.getRecent()[1], home.getRecent()[2], home.getRecent()[0]);
         this.saturday = new Shift(home.getRecent()[0], home.getRecent()[3], home.getRecent()[4]);
         this.saturdayNight = new Shift("-", "-", home.getRecent()[2]);
---
+++ src/main/resources/run.bat
@@ -0,0 +1,5 @@
+java -cp C:\Users\ranja\IdeaProjects\meal-roster\target\roster-1.0-SNAPSHOT-jar-with-dependencies.jar com.meal.Roster C:\Users\ranja\IdeaProjects\meal-roster\src\main\resources C:\Users\ranja\IdeaProjects\meal-roster\src\main\resources\archive
+cd C:\Users\ranja\IdeaProjects\meal-roster\src\main\resources
+git add recent.json
+git commit -m "updating recent.json"
+git push
